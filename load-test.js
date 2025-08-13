d5993da8ad6e9a8c46b8d370217989dba0be830a
Nguyen Tien Duy <duynguyen.das@gmail.com>
2025-08-13 08:04:18 +0000

    add tamplate email

--- load-test.js
+++ load-test.js
@@ -3,8 +3,9 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 250 }, // Tăng lên 50 người dùng ảo trong 30s
-    { duration: '2m', target: 500 },  // Giữ 50 người dùng ảo trong 1 phút
+    { duration: '1m', target: 500 }, // Tăng lên 50 người dùng ảo trong 30s
+    { duration: '2m', target: 1000 },  // Giữ 50 người dùng ảo trong 1 phút
+    { duration: '30s', target: 2000 },  // Giảm về 0 trong 30s
     { duration: '30s', target: 0 },  // Giảm về 0 trong 30s
   ],
 };
